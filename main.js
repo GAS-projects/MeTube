a3739c41bd4c904e7c535222f713db5834d4bf3d
TETTSUN <128761935+GAS-projects@users.noreply.github.com>
2024-12-20 21:55:04 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function showYoutube(){
+    var url = document.getElementById('youtubeURL_input').value;
+    if(url.substr(0,17) == "https://youtu.be/"){
+        url = url.substr(17,11);
+    }
+    else if(url.substr(0,30) == "https://www.youtube.com/embed/"){
+        url = url.substr(30,11);
+    }
+    else{
+        url = url.substr(url.indexOf("=") + 1,11);
+    }
+    document.getElementById('youtubeURL_output').src = "https://www.youtube.com/embed/" + url;
+    console.log(url);
+}
